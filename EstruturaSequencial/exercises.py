e812b5dac95f72aee72babdc9186e15d2b6d6e83
Luiza Matsuda <luiza@folhacerta.com>
2021-12-07 16:58:28 -0300

    Converting temperatures, used math function and decimal numbers

--- EstruturaSequencial/exercises.py
+++ EstruturaSequencial/exercises.py
@@ -8,6 +8,8 @@ print('O numero digitado foi: ', number)
 '''
 
 # 3 - Faça um Programa que peça dois números e imprima a soma.
+import math
+
 '''
 firstNumber = input('Digite um numero para a soma: ')
 secondNumber = input('Digite o segundo numero: ')
@@ -42,11 +44,53 @@ print('%s m -> %f cm' % (meters, centimeters))
 '''
 
 # 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+'''
+radius = input('Digite o raio do circulo, para calcular a área: ')
+area = math.pi * int(radius) ** 2
 
-#https://wiki.python.org.br/EstruturaSequencial
+print('A área do circulo e ' + str(area))
+'''
 
 # 7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+'''
+size = input('Digite o tamanho do quadrado: ')
+area = int(size) ** 4
+
+print('O dobro da area desejada e ', area)
+'''
+
 # 8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+'''
+valueHour = input('Digite o valor que voce ganha por hora: ')
+workedHours = input('Digite a quantidade de horas trabalhadas no mes: ')
+
+salary = float(valueHour) * float(workedHours)
+
+print('Seu salario sera R$', salary)
+'''
+
 # 9 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 #C = 5 * ((F-32) / 9).
+
+'''
+fahrenheit = input('Digite a temperetura em Fahrenheit: ')
+celsius = 5 * ((float(fahrenheit) - 32) / 9)
+
+print('%s°F em Celsius e %.2f°C' % (fahrenheit, celsius))
+'''
+
 # 10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+# °F = °C × 1, 8 + 32
+
+'''
+
+celsius = input('Digite a temperatura em Celsius: ')
+fahrenheit = float(celsius) * 1.8 + 32
+
+print('%s°C em Celsius e %.2f°F' % (celsius, fahrenheit))
+
+'''
+
+
+#https://wiki.python.org.br/EstruturaSequencial
